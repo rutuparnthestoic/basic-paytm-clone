89547517584bcfd4d4711f4e86fb862f5275801a
Rutuparn <rutuparnthestoic@gmail.com>
2025-12-19 15:43:54 +0530

    Fix CORS issues by adding headers to vercel.json and reordering middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 const app = express();
-  
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
+
 
 import { router } from './routes/index.js';
 //All our requests will go to router. /api/v1/{anything}
